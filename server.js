66884728853233d38b4d24cb3287c765bf46133a
Fatih Çınar <fthcnr1907@gmail.com>
2020-03-27 11:53:24 +0300

    Initial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const socketio = require('socket.io');
 
-
+// Server bileşenleri
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server)
@@ -16,6 +16,8 @@ io.on('connection', socket => {
     console.log('Yeni Bağlantı...');
 })
 
+// Port 
 const PORT = 3000 || process.env.PORT;
 
+// Server'ı kullanılmaya hazırlar
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
