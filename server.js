d0cb10a1dfe9595a54a1b65fb3c7a851d5532ff4
Fatih Çınar <fthcnr1907@gmail.com>
2020-03-28 15:19:57 +0300

    Initial commit

--- server.js
+++ server.js
@@ -3,17 +3,23 @@ const path = require('path');
 const express = require('express');
 const socketio = require('socket.io');
 
-// Server bileşenleri
+// Server Bileşenleri
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server)
+
+// Date Bileşenleri
+const date = new Date();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
 
 // Static dosyamızı tanımlıyoruz
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Kullanıcı bağlandığında çalışır
 io.on('connection', socket => {
-    console.log('Yeni Bağlantı...');
+    console.log(`Yeni Bağlantı... (${hours}:${minutes})` );
 })
 
 // Port 
